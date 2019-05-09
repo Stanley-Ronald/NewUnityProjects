a7fbecb92d22bf9096f313058d74c630319c6f64
Ronald Stanley <stanleyronaldc@gmail.com>
2019-05-09 13:32:03 -0700

    Initial commit for first person shooter game and setup project w/ gitignore file

---
+++ FirstPersonShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculate movemenet velocity as a 3D vector
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        //Final movement vector
+        Vector3 _velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        //Apply movement
+        motor.Move(_velocity);
+
+        //Calculate rotation as a 3D vector (turning around)
+        float yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(rotation);
+
+        //Calculate camera rotation as a 3D vector (turning around)
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 cameraRotation = new Vector3(xRotation, 0f, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.RotateCamera(cameraRotation);
+    }
+}
